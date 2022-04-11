5a3f5e98474f6c1a5a908d3a615b66e6fdbb3d46
leo_xiao <leo_xiao@askey.com>
2022-04-11 17:39:06 +0800

    test2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'hello !' + global.port });
+  res.render('index', { title: 'hello 2 !' + global.port });
 });
 
 module.exports = router;
