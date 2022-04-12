db4b9eca1efe4b4caa357476773192f17cc84402
leo_xiao <leo_xiao@askey.com>
2022-04-12 11:36:03 +0800

    webhook 6

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'hello 5 webhook!' + global.port });
+  res.render('index', { title: 'hello 6 webhook!' + global.port });
 });
 
 module.exports = router;
