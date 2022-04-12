d35e97544cf3e5f22f4390bd7f64e638441470f4
leo_xiao <leo_xiao@askey.com>
2022-04-12 10:56:05 +0800

    webhook 测试

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'hello 2 !' + global.port });
+  res.render('index', { title: 'hello 3 webhook!' + global.port });
 });
 
 module.exports = router;
